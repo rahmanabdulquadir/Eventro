413ee972b0cb3aa871a2ebe59b0022f3d0e9801a
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-20 21:50:21 +0600

    all sort of types added

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -50,8 +50,34 @@ export async function POST(req: Request) {
   // For this guide, you simply log the payload to the console
   const { id } = evt.data
   const eventType = evt.type
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
+ 
+
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    }
+
+    // const newUser = await createUser(user);
+
+    // if(newUser) {
+    //   await clerkClient.users.updateUserMetadata(id, {
+    //     publicMetadata: {
+    //       userId: newUser._id
+    //     }
+    //   })
+    // }
+
+    // return NextResponse.json({ message: 'OK', user: newUser })
+  }
+
+
 
   return new Response('', { status: 200 })
 }
