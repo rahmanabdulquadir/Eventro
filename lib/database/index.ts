a45be77845c56498f741fac23ce9d20e6b9455ea
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-20 23:05:13 +0600

    bug fixed

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
 const MONGODB_URI = process.env.MONGODB_URI
+console.log(MONGODB_URI)
 
 let cached = (global as any).mongoose || {connection: null, promise: null}
 
