96df27403e17f7e9635afd6af66a6132ee7df549
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-20 21:43:42 +0600

    webhook clerk api route addressed

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,13 +1,16 @@
-// type CollectionProps = {
-//   data: IEvent[],
-//   emptyTitle: string,
-//   emptyStateSubtext: string,
-//   limit: number,
-//   page: number | string,
-//   totalPages?: number,
-//   urlParamName?: string,
-//   collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
-// }
+import { IEvent } from "@/lib/database/models/event.model";
+import { Pagination } from "../ui/pagination";
+
+type CollectionProps = {
+  data: IEvent[],
+  emptyTitle: string,
+  emptyStateSubtext: string,
+  limit: number,
+  page: number | string,
+  totalPages?: number,
+  urlParamName?: string,
+  collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
+}
 
 const Collection = ({
   data,
